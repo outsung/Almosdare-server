82ef793570386c336ab73a3e6af02a245ebc44f7
outsung <insung9546@gmail.com>
2020-09-29 18:08:54 +0900

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,12 @@ const Jwt = require("jsonwebtoken");
 
 function include_jwt_info(req, res, next){
     const authorization = req.headers.authorization;
+    console.log(authorization);
     if(!authorization || authorization.indexOf("Bearer ") === -1){ req.jwt_user_idx = null; return next(); }
 
     const token = authorization.split(" ")[1];
     
-    console.log(authorization);
+    
 
     Jwt.verify(token, process.env.AUTH_SALT, (err, jwt_user) => {
         if(err){ req.jwt_user_idx = null; return next(); }
