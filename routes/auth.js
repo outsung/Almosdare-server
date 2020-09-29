22033edf46af9c218195469ac0ff3d9082826216
outsung <insung9546@gmail.com>
2020-09-29 18:16:08 +0900

    auth fix

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,11 @@ const Jwt = require("jsonwebtoken");
 
 function include_jwt_info(req, res, next){
     const authorization = req.headers.authorization;
-    console.log(authorization);
+
     if(!authorization || authorization.indexOf("Bearer ") === -1){ req.jwt_user_idx = null; return next(); }
 
     const token = authorization.split(" ")[1];
     
-    
-
     Jwt.verify(token, process.env.AUTH_SALT, (err, jwt_user) => {
         if(err){ req.jwt_user_idx = null; return next(); }
         if(!jwt_user.idx){ req.jwt_user_idx = null; return next(); }
