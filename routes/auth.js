78bfb760cc131c9627a9d1395d5bacebc5c57f5b
outsung <insung9546@gmail.com>
2020-09-30 18:46:02 +0900

    auth test

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ function include_jwt_info(req, res, next){
     console.log("token => ", token);
     
     Jwt.verify(token, process.env.AUTH_SALT, (err, jwt_user) => {
-        console.log("Jwt.verify", err, jwt_user.idx);
+        console.log("Jwt.verify", err, jwt_user);
         if(err){ req.jwt_user_idx = null; return next(); }
         
         req.jwt_user_idx = jwt_user.idx | null;
