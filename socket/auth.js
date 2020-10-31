6747616c299d7e8d8af423ac35d95f5ab4c28936
outsung <insung9546@gmail.com>
2020-10-31 11:35:41 +0900

    added socket

---
+++ socket/auth.js
@@ -0,0 +1,21 @@
+
+// Require
+const Jwt = require("jsonwebtoken");
+
+function include_jwt_info(req, res, next){
+    const authorization = req.authorization;
+
+    if(!authorization || authorization.indexOf("Bearer ") === -1){ req.jwt_user_idx = null; return next(); }
+
+    const token = authorization.split(" ")[1];
+    
+    Jwt.verify(token, process.env.AUTH_SALT, (err, jwt_user) => {
+        if(err){ req.jwt_user_idx = null; return next(); }
+        
+        req.jwt_user_idx = jwt_user.idx || null;
+        next();
+    });
+}
+
+
+module.exports = [include_jwt_info];
--- socket/socket.js
+++ socket/socket.js
@@ -1,12 +1,27 @@
+const Auth = require('./auth');
 
-const member_location = [];
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
 
 module.exports = (io) => {
+    io.use(wrap(Auth));
     io.on("connection", (socket) => {
-        console.log("Socket initiated!");
+        console.log(`[log] socket_initiated : {idx: ${socket.request.jwt_user_idx}}`);
+        socket.on("dare_init", (dare_idx) => {
+            socket.join(dare_idx);
+            io.to(dare_idx).emit("creat_user", socket.request.jwt_user_idx);
+        });
+        socket.on("instant_init", (instant_idx) => {
+            socket.join(instant_idx);
+            io.to(instant_idx).emit("creat_user", socket.request.jwt_user_idx);
+        });
+
         socket.on("getMemberLocation", (data) => {
-            console.log('got request');
-            io.emit("sendMemberLocation", data);
+            console.log(`[log] socket_getMemberLocation : {idx: ${socket.request.jwt_user_idx}, data: ${data}}`);
+            let _io = io;
+            for(let i = 0; i < rooms; i++){
+                _io = _io.to(rooms[i]);
+            }
+            _io.emit("sendMemberLocation", data);
         });
     });
 }
