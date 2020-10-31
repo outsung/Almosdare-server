7ceb6d954ab5d56f534a81ea79b588beaeb5eeb2
outsung <insung9546@gmail.com>
2020-11-01 03:22:49 +0900

    test socket auth handshake

--- socket/auth.js
+++ socket/auth.js
@@ -2,8 +2,11 @@
 // Require
 const Jwt = require("jsonwebtoken");
 
-function include_jwt_info(req, res, next){
-    const authorization = req.handshake.query.auth;
+function include_jwt_info(socket, next){
+    console.log(`req.handshake: ${socket.handshake}`);
+
+    // if(socket.handshake["query"])
+    const authorization = socket.handshake.query.auth;
     console.log(`auth : ${authorization}`);
 
     if(!authorization || authorization.indexOf("Bearer ") === -1){ req.jwt_user_idx = null; return next(); }
