25234fe60c710658917486d42754e417950610b1
outsung <insung9546@gmail.com>
2020-11-02 03:43:32 +0900

    test socket auth

--- socket/auth.js
+++ socket/auth.js
@@ -3,19 +3,18 @@
 const Jwt = require("jsonwebtoken");
 
 function include_jwt_info(socket, next){
-    console.log(`req.handshake: `, socket.handshake);
-
     const authorization = socket.handshake.query.auth;
-    console.log(`auth :`, authorization);
 
-    if(!authorization || authorization.indexOf("Bearer%20") === -1){ socket.jwt_user_idx = null; return next(); }
+    if(!authorization || authorization.indexOf("Bearer ") === -1){ socket.jwt_user_idx = null; return next(); }
 
-    const token = authorization.split("%20")[1];
+    const token = authorization.split(" ")[1];
     
+    console.log("token: ", token);
     Jwt.verify(token, process.env.AUTH_SALT, (err, jwt_user) => {
         if(err){ socket.jwt_user_idx = null; return next(); }
         
         socket.jwt_user_idx = jwt_user.idx || null;
+        console.log("token_user: ", jwt_user);
         next();
     });
 }
