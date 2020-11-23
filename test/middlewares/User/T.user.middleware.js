834578ce2d26eec7a8d92dafd92cd16f93481605
outsung <insung9546@gmail.com>
2020-11-24 04:35:42 +0900

    test added user middleware get method

--- test/middlewares/User/T.user.middleware.js
+++ test/middlewares/User/T.user.middleware.js
@@ -1,6 +1,10 @@
 // require
 const Base = require("../baseMiddlewareTest");
 const UserMiddleware = require("../../../src/middlewares/User/user.middleware");
+const UserModel = require("../../../src/models/User/user.model");
+const Chai = require('chai');
+const Sinon = require('sinon');
+
 
 // const Jwt = require("jsonwebtoken");
 // const Crypto = require("crypto");
@@ -8,78 +12,290 @@ const UserMiddleware = require("../../../src/middlewares/User/user.middleware");
 
 describe('======== UserMiddleware 테스트 ========', function(){
 
-    describe('getUserByJwt 테스트', function(){
-        const requestForm = {
+    describe('==== getUserByJwt 테스트 ====', function(){
+        const basicRequest = {
             jwt_user_idx: "token",
             body: {},
             params: {}
         };
-        const responseForm = {
-            result: "",
+        const basicResponse = {
+            status: 200,
+            json: {
+                result: 1,
+                idx: "some idx",
+                id: "some id",
+                nickname: "some nickname",
+                profileImageUrl: "some profileImageUrl"
+            }
         };
 
-        it('Action', function(){
+        describe('Base Verify', () => Base.middlewareVerifyTest(UserMiddleware.getUserByJwt[0], basicRequest));
 
-            // Base.middlewareActionTest(UserMiddleware.getUserByJwt[1], );
-            
-            /*
-                res에 result가 있는가?;
+        // Base.middlewareActionTest(UserMiddleware.getUserByJwt[1], basicResponse);
+        describe('Action', function(){
+            let stubResponse, spyNext;
 
-            */
+            before(() => {
+                stubResponse = { 
+                    status: Sinon.stub(),
+                    json: Sinon.stub()
+                };
+                stubResponse.status.returns(stubResponse);
+                spyNext = Sinon.spy();
+            });
 
+            describe('Expect', function(){
 
-            /* success
+                describe('유효한 요청이 들어왔을 때', function(){
+                    before(async () => {
+                        Sinon.stub(UserModel.Schema, 'findById').returns({
+                            _id: "some idx",
+                            id: "some id",
+                            nickname: "some nickname",
+                            profileImageUrl: "some profileImageUrl"
+                        });
+                        await UserMiddleware.getUserByJwt[1](basicRequest, stubResponse, spyNext);
+                    });
+                    after(() => { Sinon.restore(); });
 
-            validResponse = {
-                "result": 1,
-                "idx": "5f9c7ced7e18780017f4f8fd",
-                "id": "01012345678",
-                "nickname": "test"
-            }
-            */
+                    it('사용되는 쿼리는 정해져 있다', function(){
+                        Sinon.assert.calledWith(UserModel.Schema.findById, basicRequest.jwt_user_idx);
+                    });
+                    it('next()는 호출돼선 안된다', function(){
+                        Chai.expect(spyNext.notCalled).to.be.true;
+                    });
+                    it('stubResponse.status, stubResponse.json은 정해져 있다', function(){
+                        Sinon.assert.calledWith(stubResponse.status, basicResponse.status);
+                        Sinon.assert.calledWith(stubResponse.json, basicResponse.json);
+                    });
+                });
 
-            /* fail
+            });
 
-            {
-                유저 idx 가 있는경우
-            }
+            describe('Exception', function(){
 
-            */
-            UserMiddleware.getUserByJwt[1]()
+                describe('UserModel.Schema.findById이 null일 때', function(){
+                    const exceptionResponse = {
+                        status: 200,
+                        json: {
+                            result: -1,
+                            message: "Can't find anyone"
+                        }
+                    };
+
+                    before(async () => {
+                        Sinon.stub(UserModel.Schema, 'findById').returns(null);
+                        await UserMiddleware.getUserByJwt[1](basicRequest, stubResponse, spyNext);
+                    });
+                    after(() => { Sinon.restore(); });
+    
+                    it('사용되는 쿼리는 정해져 있다', function(){
+                        Sinon.assert.calledWith(UserModel.Schema.findById, basicRequest.jwt_user_idx);
+                    });
+                    it('next()는 호출돼선 안된다', function(){
+                        Chai.expect(spyNext.notCalled).to.be.true;
+                    });
+                    it('stubResponse.status, stubResponse.json은 정해져 있다', function(){
+                        Sinon.assert.calledWith(stubResponse.status, exceptionResponse.status);
+                        Sinon.assert.calledWith(stubResponse.json, exceptionResponse.json);
+                    });
+                });
+
+            });
         });
-
-        context('Verify', () => Base.middlewareVerifyTest(UserMiddleware.getUserByJwt[0], requestForm));
 
     });
 
-    describe('getUserById 테스트', function(){
-        const requestForm = {
+    describe('==== getUserById 테스트 ====', function(){
+        const basicRequest = {
             jwt_user_idx : "token",
             body: {},
             params: {
-                id: "test_id"
+                id: "some id"
+            }
+        };
+        const basicResponse = {
+            status: 200,
+            json: {
+                result: 1,
+                idx: "some idx",
+                id: "some id",
+                nickname: "some nickname",
+                profileImageUrl: "some profileImageUrl"
             }
         };
         
-        it('Action');
+        describe('Base Verify', function(){ Base.middlewareVerifyTest(UserMiddleware.getUserById[0], basicRequest); });
+    
+        // Base.middlewareActionTest(UserMiddleware.getUserById[1], basicResponse);
+        describe('Action', function(){
+            let stubResponse, spyNext;
 
-        context('Verify', function(){
-            Base.middlewareVerifyTest(UserMiddleware.getUserById[0], requestForm);
+            before(() => {
+                stubResponse = { 
+                    status: Sinon.stub(),
+                    json: Sinon.stub()
+                };
+                stubResponse.status.returns(stubResponse);
+                spyNext = Sinon.spy();
+            });
+
+            describe('Expect', function(){
+
+                describe('유효한 요청이 들어왔을 때', function(){
+                    before(async () => {
+                        Sinon.stub(UserModel.Schema, 'findOne').returns({
+                            _id: "some idx",
+                            id: "some id",
+                            nickname: "some nickname",
+                            profileImageUrl: "some profileImageUrl"
+                        });
+                        await UserMiddleware.getUserById[1](basicRequest, stubResponse, spyNext);
+                    });
+                    after(() => { Sinon.restore(); });
+
+                    it('사용되는 쿼리는 정해져 있다', function(){
+                        Sinon.assert.calledWith(UserModel.Schema.findOne, {id: basicRequest.params.id});
+                    });
+                    it('next()는 호출돼선 안된다', function(){
+                        Chai.expect(spyNext.notCalled).to.be.true;
+                    });
+                    it('stubResponse.status, stubResponse.json은 정해져 있다', function(){
+                        Sinon.assert.calledWith(stubResponse.status, basicResponse.status);
+                        Sinon.assert.calledWith(stubResponse.json, basicResponse.json);
+                    });
+                });
+
+            });
+
+            describe('Exception', function(){
+
+                describe('UserModel.Schema.findOne이 null일 때', function(){
+                    const exceptionResponse = {
+                        status: 200,
+                        json: {
+                            result: -1,
+                            message: "Can't find anyone"
+                        }
+                    };
+
+                    before(async () => {
+                        Sinon.stub(UserModel.Schema, 'findOne').returns(null);
+                        await UserMiddleware.getUserById[1](basicRequest, stubResponse, spyNext);
+                    });
+                    after(() => { Sinon.restore(); });
+
+                    it('사용되는 쿼리는 정해져 있다', function(){
+                        Sinon.assert.calledWith(UserModel.Schema.findOne, {id: basicRequest.params.id});
+                    });
+                    it('next()는 호출돼선 안된다', function(){
+                        Chai.expect(spyNext.notCalled).to.be.true;
+                    });
+                    it('stubResponse.status, stubResponse.json은 정해져 있다', function(){
+                        Sinon.assert.calledWith(stubResponse.status, exceptionResponse.status);
+                        Sinon.assert.calledWith(stubResponse.json, exceptionResponse.json);
+                    });
+                });
+
+            });
         });
+
     });
 
-    describe('getUserByIdx 테스트', function(){
-        const requestForm = {
+    describe('==== getUserByIdx 테스트 ====', function(){
+        const basicRequest = {
             jwt_user_idx : "token",
             body: {},
             params: {
-                idx: "test_idx"
+                idx: "some idx"
             }
         };
-        context('Verify', function(){
-            Base.middlewareVerifyTest(UserMiddleware.getUserByIdx[0], requestForm);
+        const basicResponse = {
+            status: 200,
+            json: {
+                result: 1,
+                idx: "some idx",
+                id: "some id",
+                nickname: "some nickname",
+                profileImageUrl: "some profileImageUrl"
+            }
+        };
+
+        describe('Verify', function(){ Base.middlewareVerifyTest(UserMiddleware.getUserByIdx[0], basicRequest); });
+
+        // Base.middlewareActionTest(UserMiddleware.getUserByIdx[1], basicResponse);
+        describe('Action', function(){
+            let stubResponse, spyNext;
+
+            before(() => {
+                stubResponse = { 
+                    status: Sinon.stub(),
+                    json: Sinon.stub()
+                };
+                stubResponse.status.returns(stubResponse);
+                spyNext = Sinon.spy();
+            });
+
+            describe('Expect', function(){
+
+                describe('유효한 요청이 들어왔을 때', function(){
+                    before(async () => {
+                        Sinon.stub(UserModel.Schema, 'findById').returns({
+                            _id: "some idx",
+                            id: "some id",
+                            nickname: "some nickname",
+                            profileImageUrl: "some profileImageUrl"
+                        });
+                        await UserMiddleware.getUserByIdx[1](basicRequest, stubResponse, spyNext);
+                    });
+                    after(() => { Sinon.restore(); });
+
+                    it('사용되는 쿼리는 정해져 있다', function(){
+                        Sinon.assert.calledWith(UserModel.Schema.findById, basicRequest.params.idx);
+                    });
+                    it('next()는 호출돼선 안된다', function(){
+                        Chai.expect(spyNext.notCalled).to.be.true;
+                    });
+                    it('stubResponse.status, stubResponse.json은 정해져 있다', function(){
+                        Sinon.assert.calledWith(stubResponse.status, basicResponse.status);
+                        Sinon.assert.calledWith(stubResponse.json, basicResponse.json);
+                    });
+                });
+
+            });
+
+            describe('Exception', function(){
+
+                describe('UserModel.Schema.findById이 null일 때', function(){
+                    const exceptionResponse = {
+                        status: 200,
+                        json: {
+                            result: -1,
+                            message: "Can't find anyone"
+                        }
+                    };
+
+                    before(async () => {
+                        Sinon.stub(UserModel.Schema, 'findById').returns(null);
+                        await UserMiddleware.getUserByIdx[1](basicRequest, stubResponse, spyNext);
+                    });
+                    after(() => { Sinon.restore(); });
+
+                    it('사용되는 쿼리는 정해져 있다', function(){
+                        Sinon.assert.calledWith(UserModel.Schema.findById, basicRequest.params.idx);
+                    });
+                    it('next()는 호출돼선 안된다', function(){
+                        Chai.expect(spyNext.notCalled).to.be.true;
+                    });
+                    it('stubResponse.status, stubResponse.json은 정해져 있다', function(){
+                        Sinon.assert.calledWith(stubResponse.status, exceptionResponse.status);
+                        Sinon.assert.calledWith(stubResponse.json, exceptionResponse.json);
+                    });
+                });
+
+            });
         });
-        it('Action');
+        
     });
 
 });
--- test/middlewares/baseMiddlewareTest.js
+++ test/middlewares/baseMiddlewareTest.js
@@ -1,5 +1,5 @@
 // require
-const Sinon = require("sinon");
+const Sinon = require('sinon');
 const Chai = require('chai');
 
 
@@ -7,136 +7,86 @@ const Chai = require('chai');
 function ifFieldDoseNotExist(middleware, badRequest, validResponse){
     const spyNext = Sinon.spy();
     const stubResponse = {
-        status: function(status){
-            this.status = status;
-            return this;
-        },
-        json: function(json){
-            this.json = json;
-            return this;
-        }
+        status: Sinon.stub(),
+        json: Sinon.stub(),
     };
+    stubResponse.status.returns(stubResponse);
 
     middleware(badRequest, stubResponse, spyNext);
     
-    it('필드가 누락되었을 때 next()는 한번도 호출돼선 안된다', function(){
+    it('next()는 한번도 호출돼선 안된다', function(){
         Chai.expect(spyNext.notCalled).to.be.true;
     });
 
-    it('필드가 누락되었을 때 필드에 따른 Response을 줘야한다', function(){
-        Chai.expect(stubResponse.status === validResponse.status).to.be.true;
-        Chai.expect(JSON.stringify(stubResponse.json) === JSON.stringify(validResponse.json)).to.be.true;
+    it('필드에 따른 Response을 줘야한다', function(){
+        Sinon.assert.calledWith(stubResponse.status, validResponse.status);
+        Sinon.assert.calledWith(stubResponse.json, validResponse.json);
     });
 }
 
 // Verify 미들웨어의 테스트 함수
 function middlewareVerifyTest(middleware, requestForm){
     
-    it('유효한 요청이 들어왔을때 next()는 한번만 호출해야한다', function(){
-        const spyNext = Sinon.spy();
-
-        middleware(requestForm, {}, spyNext);
-        Chai.expect(spyNext.calledOnce).to.be.true;
-    })
-    
-    // req.jwt_user_idx
-    if(Object.keys(requestForm).includes("jwt_user_idx")){
-        // jwt_user_idx가 존재하지 않는 badRequest;
-        const badRequest = {
-            body: {},
-            params: {}
-        };
-        Object.assign(badRequest.body, requestForm.body);
-        Object.assign(badRequest.params, requestForm.params);
+    context('Expect', function(){
+        context('유효한 요청이 들어왔을 때', function(){
+            it('next()는 한번만 호출해야한다', function(){
+                const spyNext = Sinon.spy();
         
-        context('jwt_user_idx 핃드 누락', function(){
-            ifFieldDoseNotExist(middleware, badRequest, {
-                status: 401,
-                json: "Available after login"
-            });
-        });
-    }
+                middleware(requestForm, {}, spyNext);
+                Chai.expect(spyNext.calledOnce).to.be.true;
+            })
+        })
+    });
     
-    // req.body
-    Object.keys(requestForm).map(bodyOrParams => {
-        if(!["body", "params"].includes(bodyOrParams)) return;
-
-        Object.keys(requestForm[bodyOrParams]).map(field => {
-            const badRequest = {};
-            const badObject = {};
-            Object.assign(badRequest, requestForm);
-            Object.assign(badObject, requestForm[bodyOrParams]);
-
-            delete badObject[field];
-            badRequest[bodyOrParams] = badObject;
-            context(`${field} 핃드 누락`, function(){
+    context('Exception', function(){
+        // req.jwt_user_idx
+        if(Object.keys(requestForm).includes("jwt_user_idx")){
+            // jwt_user_idx가 존재하지 않는 badRequest;
+            const badRequest = {
+                body: {},
+                params: {}
+            };
+            Object.assign(badRequest.body, requestForm.body);
+            Object.assign(badRequest.params, requestForm.params);
+            
+            context('jwt_user_idx 핃드 누락', function(){
                 ifFieldDoseNotExist(middleware, badRequest, {
-                    status: 200,
-                    json: {
-                        result: -1,
-                        message: `${field} : Field is empty`,
-                    }
+                    status: 401,
+                    json: "Available after login"
                 });
             });
-        });
+        }
+        
+        // req.body
+        Object.keys(requestForm).map(bodyOrParams => {
+            if(!["body", "params"].includes(bodyOrParams)) return;
 
+            Object.keys(requestForm[bodyOrParams]).map(field => {
+                const badRequest = {};
+                const badObject = {};
+                Object.assign(badRequest, requestForm);
+                Object.assign(badObject, requestForm[bodyOrParams]);
+
+                delete badObject[field];
+                badRequest[bodyOrParams] = badObject;
+                context(`${field} 핃드 누락`, function(){
+                    ifFieldDoseNotExist(middleware, badRequest, {
+                        status: 200,
+                        json: {
+                            result: -1,
+                            message: `${field} : Field is empty`,
+                        }
+                    });
+                });
+            });
+
+        });
     });
 
 }
 
 // Action 미들웨어의 테스트 함수
-function middlewareActionTest(middleware, requestForm){
-    
-    it('유효한 요청이 들어왔을때 next()는 한번만 호출해야한다', function(){
-        const spyNext = Sinon.spy();
-
-        middleware(requestForm, {}, spyNext);
-        Chai.expect(spyNext.calledOnce).to.be.true;
-    })
-    
-    // req.jwt_user_idx
-    if(Object.keys(requestForm).includes("jwt_user_idx")){
-        // jwt_user_idx가 존재하지 않는 badRequest;
-        const badRequest = {
-            body: {},
-            params: {}
-        };
-        Object.assign(badRequest.body, requestForm.body);
-        Object.assign(badRequest.params, requestForm.params);
-        
-        context('jwt_user_idx 핃드 누락', function(){
-            ifFieldDoseNotExist(middleware, badRequest, {
-                status: 401,
-                json: "Available after login"
-            });
-        });
-    }
-    
-    // req.body
-    Object.keys(requestForm).map(bodyOrParams => {
-        if(!["body", "params"].includes(bodyOrParams)) return;
-
-        Object.keys(requestForm[bodyOrParams]).map(field => {
-            const badRequest = {};
-            const badObject = {};
-            Object.assign(badRequest, requestForm);
-            Object.assign(badObject, requestForm[bodyOrParams]);
-
-            delete badObject[field];
-            badRequest[bodyOrParams] = badObject;
-            context(`${field} 핃드 누락`, function(){
-                ifFieldDoseNotExist(middleware, badRequest, {
-                    status: 200,
-                    json: {
-                        result: -1,
-                        message: `${field} : Field is empty`,
-                    }
-                });
-            });
-        });
-
-    });
-
+function middlewareActionTest(){
 }
 
 module.exports = {
